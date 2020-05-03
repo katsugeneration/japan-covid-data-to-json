e9952e7028248e77a7168d80cea589d1fb941db5
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2020-05-03 18:01:06 +0900

    Fix

--- process_moving.py
+++ process_moving.py
@@ -85,6 +85,10 @@ for k, v in regions.items():
     fig = plt.figure(figsize=(16.0, 9.0))
     ax1 = fig.subplots()
     ax2 = ax1.twinx()
+
+    if v not in _df_p_week.columns():
+        _df_p_week[v] = 0
+
     ax1.bar(dates, _df_p_week[v], color='y', width=2.0, label='新規感染判明数')
     ax2.plot(dates[:-1], df_region['workplaces_percent_change_from_baseline'], marker='o', label='職場滞在指数')
     ax2.plot(dates[:-1], df_region['transit_stations_percent_change_from_baseline'], marker='o', label='公共交通機関滞在指数（電車、バス、など）')
